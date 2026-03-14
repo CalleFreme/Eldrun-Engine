33253941c6a6d1d69bd7b8b806e44dd7d6720ea7
CalleFreme <cj.freme@gmail.com>
2026-03-14 16:31:07 +0100

    feat: add editor and sandbox app skeletons

---
+++ editor/eldrun_editor/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Eldrun Editor starting...\n";
+    return 0;
+}
---
+++ games/sandbox/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Eldrun Sandbox starting...\n";
+    return 0;
+}
